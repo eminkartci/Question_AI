966ebac695f2107cd2cab8c0933a741e93f365db
eminkartci <emin.kartci@ozu.edu.tr>
2021-02-26 14:40:14 +0300

    Test object is arranged. We can save the Test to Assests/Tests print is also changed

--- Test.py
+++ Test.py
@@ -14,6 +14,9 @@ GitHub : eminkartci
     # Subject           -> Subject Object
     # QuestionCount     -> Integer
 
+## Imports
+import os # To see and change the Directiry
+
 class Test:         
 
     # Constructor
@@ -27,27 +30,50 @@ class Test:
 
 
     # Show the test at the console
-    def show_test_console(self):
+    def show_test_console(self,willPrint = True):
 
         # Create a string to prepare good format
-        testString = """TEST ID         : {}\nSubject         : {}\nQuestion Count  : {}
-        """.format(self.ID,self.Subject,self.QuestionCount)
+        testString = """TEST ID         : {}
+Subject         : {}
+Question Count  : {}
+""".format(self.ID,self.Subject,self.QuestionCount)
 
-        # Print the file to the console
-        print(testString)
+        # if the user wants to print to the console
+        if willPrint:
+            # Print the file to the console
+            print(testString)
 
         # Return the string to use another way
         return testString
 
 
     def save_test_as_txt(self):
-        testString = self.show_test_console()
-        # If willSaved is true save the Test properties as ID+".txt" file
-        testFile = open( str(self.ID)+".txt","w") 
-        testFile.write("--------> TEST OBJECT <--------\n")
-        testFile.write(testString)
-        testFile.write("\n\nAll Rights are Reserved @2021 A-MEAN Company")
-        testFile.close() 
+
+        # Try to execute these operations
+        try:
+
+            testString = self.show_test_console(False)
+
+            save_path  = os.path.join( os.getcwd()+"/Assests/Tests/", str(self.ID)+".txt")
+
+            # If willSaved is true save the Test properties as ID+".txt" file
+            testFile = open(save_path,"w") 
+
+            # Write in the file 
+            testFile.write("--------> TEST OBJECT <--------\n")
+            testFile.write(testString)
+            testFile.write("\n\nAll Rights are Reserved @2021 A-MEAN Company")
+            testFile.close() 
+            # Close the file properly
+            testFile.close() 
+
+            # Inform the user
+            print(f"The Test {self.ID} saved successfully.")
+        
+        # If any problem occurs
+        except:
+            # Inform the user !!
+            print(f"The Test {self.ID} cannot saved !! Try again ...")
 
 # DRIVER PROGRAM
 Math_Test = Test(1,["Q1","Q2","Q3"],"Math",15)
