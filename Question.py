12cb69a828f5498cd393c77f52d1be72d0a4b189
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-01 23:03:58 +0300

    Question Class is implemented

---
+++ Question.py
@@ -0,0 +1,41 @@
+
+
+# Create a Question class
+class Question:
+
+    # Constructor
+        # ID
+        # QuestionString
+        # Options
+        # Answer
+        # Subject
+        # Solution
+    def __init__(self,ID,Question):
+        self.ID = ID
+
+    # Print Question
+
+    # Assign Solution
+
+        # Ask for a solution
+
+        # Create a solution object
+
+        # Connect them
+
+        # Save the solution
+
+            # create a txt file at -> Assests -> Questions
+        
+            # write question content
+
+            # save file
+
+    # Save Question
+        # create a txt file at -> Assests -> Questions
+        
+        # write question content
+
+        # save file
+
+    
